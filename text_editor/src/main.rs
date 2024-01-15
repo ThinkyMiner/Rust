125064d51d9b944eb102991c6675e04f8272b70b
ThinkyMiner <131332283+ThinkyMiner@users.noreply.github.com>
2024-01-15 18:49:03 +0530

    User Input Completed

--- text_editor/src/main.rs
+++ text_editor/src/main.rs
@@ -1,19 +1,25 @@
-use iced::{Sandbox, widget::text, Settings};
+use iced::{widget::container, Sandbox, widget::text_editor, Settings};
 
 fn main() -> iced::Result{
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content
+}
 
-#[derive(Debug)]
-enum Message  {}
+#[derive(Debug, Clone)]
+enum Message  {
+    Edit(text_editor::Action)
+}
 
 impl Sandbox for Editor{
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self{
+            content:text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
@@ -21,10 +27,16 @@ impl Sandbox for Editor{
     }
 
     fn update(&mut self, message: Message) {
-        match message{};
+        match message{
+            Message::Edit(Action)=>{
+                //Applying the actions we intend to apply
+                self.content.edit(Action);
+            }
+        };
     }
 
     fn view(&self) -> iced::Element<'_, Message> {
-        text("Hello, World").into()
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        container(input).padding(10).into() // Check if u want padding like a box around where u type
     }
 }
